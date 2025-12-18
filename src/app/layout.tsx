50b7bd7e12978c5277611707ce66ad64187c2ce8
leila237 <mbiydzenyuyeileen@gmail.com>
2025-12-18 15:55:43 +0100

    Implemented the Refactor Global Loading task
     - removed import Loading from ./feed/loading
     - remove <Loading /> from the JSX
     - created src/app/loading.tsx.
    - Also, delete the src/feed/ directory along with loading.tsx file
    
    Bug: T413012
    Change-Id: I8045570b17d3c835802d2c5f4839dfd5b1dc11da

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Loading from "./feed/loading";
 import { ClientProviders } from "../hooks/client-providers";
 import Footer from "../components/layout/footer";
 import Header from "../components/layout/header";
@@ -11,12 +10,12 @@ import SkipNav from "../components/layout/skip-nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -24,35 +23,32 @@ export const metadata: Metadata = {
   description: "Discover Actor Collaborations through Wikidata",
   icons: {
     icon: "./favicon.ico",
-    shortcut: "./favicon.svg",
+    shortcut: "./favicon.svg"
   },
   // applicationName: "WDTMCollab",
   openGraph: {
     title: "WDTMCollab",
     description: "Discover Actor Collaborations through Wikidata",
     url: "https://wdtmcollab.toolforge.org",
-    siteName: "WDTMCollab",
-  },
+    siteName: "WDTMCollab"
+  }
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang='en' className='h-full'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
         <SkipNav />
-        <Loading />
         <ErrorBoundary>
           <ClientProviders>
             <Header />
-            <div id="main-content">
-              {children}
-            </div>
+            <div id='main-content'>{children}</div>
             <Footer />
           </ClientProviders>
         </ErrorBoundary>
